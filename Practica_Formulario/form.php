2e7deb00ca31865929a67a0091aee45921bb9193
bilal96204 <204@cifpceuta.es>
2022-11-05 15:06:05 +0100

    05/11

--- Practica_Formulario/form.php
+++ Practica_Formulario/form.php
@@ -13,6 +13,7 @@
         $regionalCode = $_POST['regionalCode'];
         $subscriptionType = $_POST['subscriptionType'];
         $subscriptionCode = $_POST['subscriptionCode'];
+        $interest = $_POST['interest'];
         $username = $_POST['username'];
         $password = $_POST['password'];
         $cardType = $_POST['cardType'];
@@ -20,44 +21,56 @@
         $discount = $_POST['discount'];
         $feedback = $_POST['feedback'];
 
+        /* meter todo en un array */
+        $data = array(
+            'title' => $title,
+            'firstName' => $firstName,
+            'lastName' => $lastName,
+            'birthdate' => $birthdate,
+            'city' => $city,
+            'region' => $region,
+            'postcode' => $postcode,
+            'regionalCode' => $regionalCode,
+            'subscriptionType' => $subscriptionType,
+            'subscriptionCode' => $subscriptionCode,
+            'interest' => $interest,
+            'username' => $username,
+            'password' => $password,
+            'cardType' => $cardType,
+            'cardNumber' => $cardNumber,
+            'discount' => $discount,
+            'feedback' => $feedback
+        );
 
-        echo "<h1>Data has been sent</h1>";
-        
+
+        echo "<h1>Data has been submitted</h1>";
 
         /* imprimir en tabla */
         echo "<table border='1'>";
-        echo "<tr><td>Title</td><td>$title</td></tr>";
-        echo "<tr><td>First Name</td><td>$firstName</td></tr>";
-        echo "<tr><td>Last Name</td><td>$lastName</td></tr>";
-        echo "<tr><td>Birthdate</td><td>$birthdate</td></tr>";
-        echo "<tr><td>City</td><td>$city</td></tr>";
-        echo "<tr><td>Region</td><td>$region</td></tr>";
-        echo "<tr><td>Postcode</td><td>$postcode</td></tr>";
-        echo "<tr><td>Regional Code</td><td>$regionalCode</td></tr>";
-        echo "<tr><td>Subscription Type</td><td>$subscriptionType</td></tr>";
-        echo "<tr><td>Subscription Code</td><td>$subscriptionCode</td></tr>";
+        foreach($data as $key => $value){
+            $firstLetter = ucfirst($key);
+            echo "<tr>";
+            echo "<td>$firstLetter</td>";
 
-        /* imprimir en lista */
-        echo "<tr><td>Interest</td><td><ul>";
-        if(!empty($_POST['interest'])){
-            foreach($_POST['interest'] as $value){
-                echo "<li>$value</li>";
+            /* guardar password encriptada */
+            if($key == 'password'){
+                $value = md5($value);
             }
+
+            if($key == 'interest'){
+                echo '<td>';
+                foreach($interest as $value){
+                    echo "<ul>";
+                    echo "<li>$value</li>";
+                    echo "</ul>";
+                }
+                echo '</td>';
+            }else{
+                echo "<td>$value</td>";
+            }
+            echo "</tr>";
         }
-        echo "</ul></td></tr>";
-
-        echo "<tr><td>Username</td><td>$username</td></tr>";
-
-        /* guardar contraseña encriptada*/
-        $password = md5($password);
-        echo "<tr><td>Password</td><td>$password</td></tr>";
-        echo "<tr><td>Card Type</td><td>$cardType</td></tr>";
-        echo "<tr><td>Card Number</td><td>$cardNumber</td></tr>";
-        echo "<tr><td>Discount</td><td>$discount</td></tr>";
-        echo "<tr><td>Feedback</td><td>$feedback</td></tr>";
         echo "</table>";
-
-        
         
     }
 ?>
