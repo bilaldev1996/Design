71a7de3a6abb4f0345f2428fafde4f991547b8ec
bilal96204 <204@cifpceuta.es>
2022-11-08 16:29:00 +0100

    form

--- contact.php
+++ contact.php
@@ -1,21 +1,35 @@
 <?php
 
-    if(isset($_POST['enviar'])) {
+    if(isset($_POST['submit'])) {
         $name = $_POST['nombre'];
+        $lastname = $_POST['lastname'];
         $email = $_POST['email'];
+        $confirmEmail = $_POST['confirmEmail'];
+        $phone = $_POST['phone'];
         $affair = $_POST['asunto'];
         $message = $_POST['mensaje'];
+
+        if($email !== $confirmEmail) {
+            echo "<script>
+                alert('The emails do not match');
+                window.history.go(-1);
+            </script>";
+        }
 
 
         echo "<h2>Your form has been submitted successfully!</h2>";
         echo "<h3>Here is the data you submitted:</h3>";
         /* mostrar en tabla */
-        echo "<table border='1' cellpadding='10' cellspacing='0' width='100%'>";
-        echo "<tr><td>Nombre</td><td>$name</td></tr>";
-        echo "<tr><td>Email</td><td>$email</td></tr>";
-        echo "<tr><td>Asunto</td><td>$affair</td></tr>";
-        echo "<tr><td>Mensaje</td><td>$message</td></tr>";
+        echo "<table border='1'>";
+        echo "<tr><td>Name:</td><td>$name</td></tr>";
+        echo "<tr><td>Last Name:</td><td>$lastname</td></tr>";
+        echo "<tr><td>Email:</td><td>$email</td></tr>";
+        echo "<tr><td>Confirm Email:</td><td>$confirmEmail</td></tr>";
+        echo "<tr><td>Phone:</td><td>$phone</td></tr>";
+        echo "<tr><td>Affair:</td><td>$affair</td></tr>";
+        echo "<tr><td>Message:</td><td>$message</td></tr>";
         echo "</table>";
+
 
     }
 
